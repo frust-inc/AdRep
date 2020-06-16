92a880a212a0fed694c68b192a425da525cba901
satoshi03 <innamisatoshi@gmail.com>
2020-06-16 21:24:05 +0900

    Add ctr, cvr, cpc, cpm

--- provider/base.py
+++ provider/base.py
@@ -36,6 +36,30 @@ class BaseAdResponse(BaseResponse):
         self.conversion = conversion
         self.used_budget = used_budget
 
+    def _calc_ctr(self):
+        if self.click > 0 and self.impression > 0:
+            return self.click / self.impression
+        else:
+            return 0.0
+
+    def _calc_cvr(self):
+        if self.conversion > 0 and self.click > 0:
+            return self.conversion / self.click
+        else:
+            return 0.0
+
+    def _calc_cpc(self):
+        if self.used_budget > 0 and self.click > 0:
+            return self.used_budget / self.click
+        else:
+            return 0.0
+
+    def _calc_cpm(self):
+        if self.used_budget > 0 and self.impression > 0:
+            return self.used_budget / self.impression * 1000
+        else:
+            return 0.0
+
     def to_dict(self):
         return {
             'provider': self.__class__.name,
@@ -44,4 +68,8 @@ class BaseAdResponse(BaseResponse):
             'click': self.click,
             'conversion': self.conversion,
             'used_budget': self.used_budget,
+            'ctr': self._calc_ctr(),
+            'cvr': self._calc_cvr(),
+            'cpc': self._calc_cpc(),
+            'cpm': self._calc_cpm(),
         }
