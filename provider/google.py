1d6d494f06397e082e7ab1030ad7d6dbfdf27239
onecto-3 <akisei1997@yahoo.co.jp>
2020-06-19 03:30:53 +0000

    google ads API useage

--- provider/google.py
+++ provider/google.py
@@ -13,36 +13,39 @@ class GoogleAds(BaseProvider):
             'developer_token': self.config['DEVELOPER_TOKEN'],
             'refresh_token': self.config['REFRESH_TOKEN'],
             'client_id': self.config['CLIENT_ID'],
-            'client_secret': self.config['CLIENT_SECRET']
+            'client_secret': self.config['CLIENT_SECRET'],
+            'login_customer_id': self.config['MCC_CUSTOMER_ID'],
             }
         
         client = google.ads.google_ads.client.GoogleAdsClient.load_from_dict(credentials)
 
-        ### debug ###
-        page_size = 1000
-        customer = "8442677458"
-        ga_service = client.get_service('GoogleAdsService', version='v1')
+        customer_id = self.config['CLIENT_CUSTOMER_ID']
+        ga_service = client.get_service('GoogleAdsService', version='v3')
 
         query = (
-                'SELECT campaign.id, campaign.name, metrics.impressions, metrics.clicks, metrics.all_conversions_value'
+                'SELECT campaign.id, campaign.name, metrics.impressions, metrics.clicks , metrics.conversions '
                 'FROM campaign'
                 )
 
-        response = ga_service.search(customer, query, page_size=page_size)
+        response = ga_service.search_stream(customer_id, query) 
+
+        ret_data=[]
 
         try:
-            for row in response:
-                campaign = row.campaign
-                metrics = row.metrics
+            for batch in response:
+                for row in batch.results:
+                    campaign = row.campaign
+                    metrics = row.metrics
 
-                print('CAMPAIGN_ID: %d'
-                      'CAMPAIGN_NAME: %s'
-                      'IMPRESSIONS: %d'
-                      'CLICKS: %d'
-                      'CONVERSIONS: %d'
-                    % (campaign.id.value, campaign.name.value,
-                       metrics.impressions.value, metrics.clicks.value, 
-                       metrics.all_conversions_value.value))
+                    ret_data += [{
+                                'campaign': campaign.name.value,
+                                'date':'YYYY/MM/DD',
+                                'impression': metrics.impressions.value,
+                                'click': metrics.clicks.value,
+                                'conversion': metrics.all_conversions_value.value,
+                                'used_budget': 0,
+                                }]
+            ret_state='ok'
 
         except google.ads.google_ads.errors.GoogleAdsException as ex:
             print('Request with ID "%s" failed with status "%s" and includes the '
@@ -52,35 +55,34 @@ class GoogleAds(BaseProvider):
                 if error.location:
                     for field_path_element in error.location.field_path_elements:
                         print('\t\tOn field: %s' % field_path_element.field_name)
+            ret_data += [{
+                        'campaign': '---',
+                        'date': '0000/00/00',
+                        'impression': -1,
+                        'click': -1,
+                        'conversion': -1,
+                        'used_budget': -1,
+                        }]
+            ret_state='ng'
 
-
-
-
-
-        #end_debug#
-
-        # Dummy
-        # TODO: Fetch from actual data provider
-        return GoogleAdsResponse('ok', {
-            'date': '2020/06/01',
-            'impression': 100000,
-            'click': 1000,
-            'conversion': 10,
-            'used_budget': 30000,
-        })
+        return GoogleAdsResponse(ret_state, ret_data)
 
     def build_reports(self, data, **kwargs):
-        return [
-            AdReport(
-                date=data['date'],
-                time=kwargs['time'],
-                name='google',
-                impression=data['impression'],
-                click=data['click'],
-                conversion=data['conversion'],
-                used_budget=data['used_budget'],
-            )
-        ]
+        ret_data = []
+        for row in data:
+            ret_data += [
+                        AdReport(
+                            date=row['date'],
+                            time=kwargs['time'],
+                            name='google '+row['campaign'],
+                            impression=row['impression'],
+                            click=row['click'],
+                            conversion=row['conversion'],
+                            used_budget=row['used_budget'],
+                            )
+                        ]
+
+        return ret_data
 
 
 class GoogleAdsResponse(BaseResponse):
