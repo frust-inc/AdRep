00b3f1bec48f41d2bec173309e0ff4038dea4a21
satoshi03 <innamisatoshi@gmail.com>
2020-06-23 00:17:32 +0900

    コマンドラインから引数を受け取り集計対象日を指定可能にする

--- scripts/run.py
+++ scripts/run.py
@@ -1,15 +1,29 @@
+import datetime
+import fire
+
 from config import load_config
 from provider import GoogleAds, TamagoRepeat
 from writer import WriterBuilder
 
 
-def main():
+def main(start_date=datetime.date.today(), end_date=datetime.date.today(),
+         start_days_before=0, end_days_before=0):
+
+    if type(start_date) == str:
+        start_date = datetime.datetime.strptime(start_date, "%Y/%m/%d")
+        end_date = datetime.datetime.strptime(end_date, "%Y/%m/%d")
+    start_date = start_date - datetime.timedelta(days=start_days_before)
+    end_date = end_date - datetime.timedelta(days=end_days_before)
+
     config = load_config("config.yaml")
-    fetch_and_update_ad_report(config)
-    fetch_and_update_shop_report(config)
+    while start_date <= end_date:
+        print(start_date)
+        fetch_and_update_ad_report(config, start_date)
+        fetch_and_update_shop_report(config, start_date)
+        start_date += datetime.timedelta(days=1)
 
 
-def fetch_and_update_ad_report(config):
+def fetch_and_update_ad_report(config, target_date):
     providers = [
         GoogleAds(config=config['AD']['INPUT']['GOOGLE']),
         # Facebook(config=config['AD']['FACEBOOK']),
@@ -26,7 +40,7 @@ def fetch_and_update_ad_report(config):
         writer.write(reports)
 
 
-def fetch_and_update_shop_report(config):
+def fetch_and_update_shop_report(config, target_date):
     shops = [
         TamagoRepeat(config=config['SHOP']['INPUT']['TAMAGO_REPEAT']),
     ]
@@ -42,4 +56,13 @@ def fetch_and_update_shop_report(config):
 
 
 if __name__ == "__main__":
-    main()
+    """
+    run.py --start-date [startdate] --end [enddate]
+    e.g. when you update report between 2020/06/01 and 2020/06/02
+    $ run.py --start-date 2020/06/01 --end-date 2020/06/02
+
+    run.py --start-days-before [start-days-before] --end-days-before [end-days-before]
+    e.g. when you update report between 10 days and yesterday.
+    $ run.py --start-days-before 10 --end-days-before 1
+    """
+    fire.Fire(main)
