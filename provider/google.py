acc17767a725a18cf8b6a97f486a8a3d10f599d3
onecto-3 <akisei1997@yahoo.co.jp>
2020-06-22 02:37:25 +0000

    flake8エラー解消

--- provider/google.py
+++ provider/google.py
@@ -1,14 +1,13 @@
 from .base import BaseProvider, BaseResponse
 from report import AdReport
-
 import google.ads.google_ads.client
+
 
 class GoogleAds(BaseProvider):
     def __init__(self, config=None):
         super(GoogleAds, self).__init__(config=config)
 
     def fetch(self):
-
         credentials = {
             'developer_token': self.config['DEVELOPER_TOKEN'],
             'refresh_token': self.config['REFRESH_TOKEN'],
@@ -16,19 +15,20 @@ class GoogleAds(BaseProvider):
             'client_secret': self.config['CLIENT_SECRET'],
             'login_customer_id': self.config['MCC_CUSTOMER_ID'],
             }
-        
+
         client = google.ads.google_ads.client.GoogleAdsClient.load_from_dict(credentials)
 
         customer_id = self.config['CLIENT_CUSTOMER_ID']
         ga_service = client.get_service('GoogleAdsService', version='v3')
 
         query = (
-                'SELECT metrics.impressions, metrics.clicks, metrics.conversions, metrics.cost_micros '
+                'SELECT metrics.impressions, metrics.clicks, '
+                'metrics.conversions, metrics.cost_micros '
                 'FROM customer '
                 'WHERE segments.date DURING TODAY '
                 )
 
-        response = ga_service.search_stream(customer_id, query) 
+        response = ga_service.search_stream(customer_id, query)
 
         try:
             for batch in response:
@@ -36,17 +36,17 @@ class GoogleAds(BaseProvider):
                     metrics = row.metrics
 
                     ret_data = {
-                                'date':'YYYY/MM/DD',
+                                'date': 'YYYY/MM/DD',
                                 'impression': metrics.impressions.value,
                                 'click': metrics.clicks.value,
                                 'conversion': metrics.all_conversions_value.value,
                                 'used_budget': metrics.cost_micros.value,
                                 }
-            ret_state='ok'
+            ret_state = 'ok'
 
         except google.ads.google_ads.errors.GoogleAdsException as ex:
             print('Request with ID "%s" failed with status "%s" and includes the '
-                    'following errors:' % (ex.request_id, ex.error.code().name))
+                  'following errors:' % (ex.request_id, ex.error.code().name))
             for error in ex.failure.errors:
                 print('\tError with message "%s".' % error.message)
                 if error.location:
@@ -59,12 +59,12 @@ class GoogleAds(BaseProvider):
                         'conversion': -1,
                         'used_budget': -1,
                         }
-            ret_state='ng'
+            ret_state = 'ng'
 
         return GoogleAdsResponse(ret_state, ret_data)
 
     def build_reports(self, data, **kwargs):
-    
+
         ret_data = [
                     AdReport(
                         date=data['date'],
