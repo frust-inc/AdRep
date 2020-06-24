d38a49588a16214f74148ea21ab546e90474946e
satoshi03 <innamisatoshi@gmail.com>
2020-06-24 16:46:11 +0900

    CloudFunctions

--- logger/__init__.py
+++ logger/__init__.py
@@ -9,10 +9,10 @@ def init_logger(config):
     root_logger = logging.getLogger()
     root_logger.setLevel(config["LOG_LEVEL"])
 
-    file_name = "run_{}.log".format(datetime.strftime(datetime.now(), "%Y%m%d_%H%M%S"))
-    file_handler = logging.FileHandler(os.path.join(config["LOG_PATH"], file_name))
-    file_handler.setFormatter(log_formatter)
-    root_logger.addHandler(file_handler)
+    # file_name = "run_{}.log".format(datetime.strftime(datetime.now(), "%Y%m%d_%H%M%S"))
+    # file_handler = logging.FileHandler(os.path.join(config["LOG_PATH"], file_name))
+    # file_handler.setFormatter(log_formatter)
+    # root_logger.addHandler(file_handler)
 
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(log_formatter)
