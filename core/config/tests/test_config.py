95b965f1cd1e8e8c3ff2262175abebe2b8f431ed
satoshi03 <innamisatoshi@gmail.com>
2020-06-30 19:59:04 +0900

    Add test

--- core/config/tests/test_config.py
+++ core/config/tests/test_config.py
@@ -3,9 +3,14 @@ import mock
 from core.config import ConfigLoader
 
 
+class DummySecretManager():
+    def get(self, key):
+        return '{}:EXPECTED'.format(key)
+
+
 def test_replace_with_env_var():
     '''
-    _replace_strが正しく値を変換できること
+    _replace_strが環境変数で正しく値を変換できること
     '''
     loader = ConfigLoader('')
 
@@ -16,6 +21,21 @@ def test_replace_with_env_var():
         assert loader._replace_str('{{ ENV:HOGE }}') == 'EXPECTED'
         assert loader._replace_str('{{ ENV:HOGE_AND_HOGE }}') == 'EXPECTED'
         assert loader._replace_str('{{ ENV:hoge }}') == 'EXPECTED'
+
+
+def test_replace_with_secret_var():
+    '''
+    _replace_strがシークレットで正しく値を変換できること
+    '''
+    secret_manager = DummySecretManager()
+    loader = ConfigLoader('', secret_manager)
+
+    assert loader._replace_str('HOGE') == 'HOGE'
+    assert loader._replace_str('{ HOGE }') == '{ HOGE }'
+    assert loader._replace_str('%{ HOGE }') == '%{ HOGE }'
+    assert loader._replace_str('{{ SECRET:HOGE }}') == 'HOGE:EXPECTED'
+    assert loader._replace_str('{{ SECRET:HOGE_AND_HOGE }}') == 'HOGE_AND_HOGE:EXPECTED'
+    assert loader._replace_str('{{ SECRET:hoge }}') == 'hoge:EXPECTED'
 
 
 def test_parse_and_replace():
